ba802b3d1317238ba5083881df00779ef21bea76
ExaltedBagel <jacobmaxime@hotmail.com>
2017-03-10 22:47:32 -0500

    Debut de map

---
+++ GameJamVR/Assets/MapAreaScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapAreaScript : MonoBehaviour {
+    public GameObject basePanel;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 16*32; ++i)
+        {
+            var newPanel = Instantiate(basePanel);
+            newPanel.transform.parent = this.transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
