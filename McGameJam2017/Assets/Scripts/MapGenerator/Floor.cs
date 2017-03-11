be497625a5db078e8b3a020c399c8a668fa6d743
RachelCoteBessette <rachel.cote-bessette@polymtl.ca>
2017-03-11 08:47:38 -0500

    RCB : ajout du dossier MapGenerator et des scripts Floor.cs, Map.cs et MapGenerator.cs.

---
+++ McGameJam2017/Assets/Scripts/MapGenerator/Floor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor {
+
+    [SerializeField]
+    static int dim1 = 16;
+    [SerializeField]
+    static int dim2 = 32;
+
+    char[][] tiles;
+
+    public Floor()
+    {
+        tiles = new char[dim1][];
+        for (int i = 0; i < dim1; i++)
+        {
+            tiles[i] = new char[dim2];
+        }
+    } 
+
+    public char[][] Tiles
+    {
+        get
+        {
+            return tiles;
+
+        }
+        set
+        {
+            tiles = value;
+        }
+    }
+}
