f5fdc7b97ea061e1b8a17a4768e9cd94406237ed
RachelCoteBessette <rachel.cote-bessette@polymtl.ca>
2017-03-11 00:41:15 -0500

    RCB: début des scripts pour un générateur de map.

---
+++ McGameJam2017/Assets/Scripts/Floor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+    [SerializeField]
+    static int dim1 = 16;
+    [SerializeField]
+    static int dim2 = 32;
+
+    char[,] tiles = new char[dim1,dim2];
+	
+	public char[,] Tiles
+    {
+        get
+        {
+            return tiles;
+
+        }
+        set
+        {
+            tiles = value;
+        }
+
+    }
+}
