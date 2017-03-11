210728107b79c0d9679e21ffb4480358d13d3615
ExaltedBagel <jacobmaxime@hotmail.com>
2017-03-11 03:36:57 -0500

    Network Discovery connect trial

---
+++ McGameJam2017/Assets/Scripts/Network/CustomNetworkDiscovery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkDiscovery : NetworkDiscovery {
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        Debug.Log(data);
+    }
+}
