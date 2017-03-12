3a40bf51218400a4a014a472eb86ab79f586a7d8
Julien Charbonneau <julien_charbonneau@live.ca>
2017-03-12 12:57:45 -0400

    GameOver et death to menu

---
+++ McGameJam2017/Assets/Scripts/Game/GameOver.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// VARIABLES
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    [SerializeField]
+    private RawImage hidePanel;
+    [SerializeField]
+    private RawImage banner;
+    [SerializeField]
+    private Text diedText;
+    [SerializeField]
+    private float fadeInSpeed;
+    [SerializeField]
+    private float delayBeforeMenu;
+
+    private AudioSource audioSource;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// UNITY
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// METHODS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void ShowGameOver(float delay) {
+        StartCoroutine(FadeInHideScreen(delay));
+    }
+
+
+    IEnumerator FadeInHideScreen(float delay) {
+        yield return new WaitForSeconds(delay);
+
+        banner.enabled = true;
+        diedText.enabled = true;
+        hidePanel.enabled = true;
+        audioSource.Play();
+
+        Color color = hidePanel.color;
+        color.a = 0.0f;
+        hidePanel.color = color;
+
+        while (color.a <= 1.0f) {
+            color.a += Time.deltaTime * fadeInSpeed;
+            hidePanel.color = color;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(delayBeforeMenu);
+        SceneManager.LoadScene("MenuScene");
+    }
+}
