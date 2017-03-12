8326a8b8a592cebc256f358be02f74f95fca3f5f
Julien Charbonneau <julien_charbonneau@live.ca>
2017-03-11 20:00:34 -0500

    TileEffects and map update

---
+++ McGameJam2017/Assets/Scripts/Game/Tiles/TileSpring.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpring : ITileEffect {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// VARIABLES
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public float launchAngleDeg = 60;
+
+    private Rigidbody playerRigidbody;
+    private SelectionOutline selectionOutline;
+    private Transform playerTransform;
+    private Vector3 targetPos;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// UNITY
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    void Awake () {
+        GameObject player = GameObject.Find("PlayerVR");
+        playerRigidbody = player.GetComponent<Rigidbody>();
+        selectionOutline = player.GetComponent<SelectionOutline>();
+        playerTransform = player.transform;
+	}
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// METHODS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public override void PlayEffect() {
+        targetPos = playerTransform.position + selectionOutline.Direction * tileWidth * 2.0f;
+        Vector3 launchImpulse = LaunchVelocity(targetPos, launchAngleDeg);
+        playerRigidbody.useGravity = true;
+        playerRigidbody.isKinematic = false;
+        playerRigidbody.AddForce(launchImpulse, ForceMode.Impulse);
+
+        StartCoroutine(TrackPlayerJump());
+    }
+
+
+    private Vector3 LaunchVelocity(Vector3 targetPos, float angle) {
+        Vector3 dir = targetPos - playerTransform.position;
+        float height = dir.y;
+        dir.y = 0;
+        float distance = dir.magnitude;
+        float radAngle = angle * Mathf.Deg2Rad;
+        dir.y = distance * Mathf.Tan(radAngle);
+        distance += height / Mathf.Tan(radAngle);
+        float velocity = Mathf.Sqrt(distance * Physics.gravity.magnitude / Mathf.Sin(2 * radAngle));
+        return velocity * dir.normalized;
+    }
+
+
+    IEnumerator TrackPlayerJump() {
+        while (playerTransform.position.y >= targetPos.y) {
+            yield return null;
+        }
+
+        playerRigidbody.useGravity = false;
+        playerRigidbody.isKinematic = true;
+
+        playerTransform.position = targetPos;
+    }
+}
---
+++ McGameJam2017/Assets/Scripts/Game/Tiles/TileTeleportUp.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTeleportUp : ITileEffect {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// VARIABLES
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    enum TeleportType {
+        Down = -1,
+        Up = 1
+    }
+
+    [SerializeField]
+    private TeleportType teleportType;
+    private float teleportDelay;
+    private Transform playerTransform;
+    private Transform linkedTeleporter;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// UNITY
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    void Awake() {
+        GameObject player = GameObject.Find("PlayerVR");
+        playerTransform = player.transform;
+    }
+
+    void Start() {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0F);
+
+        for (int i = 0; i < hits.Length; i++) {
+            RaycastHit hit = hits[i];
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+
+            if (rend) {
+                // Change the material of all hit colliders
+                // to use a transparent shader.
+                rend.material.shader = Shader.Find("Transparent/Diffuse");
+                Color tempColor = rend.material.color;
+                tempColor.a = 0.3F;
+                rend.material.color = tempColor;
+            }
+        }
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// METHODS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public override void PlayEffect() {
+        StartCoroutine(TeleportAfterDelay());
+    }
+
+
+    private void FindLinkedTeleporter() {
+
+    }
+
+
+    private IEnumerator TeleportAfterDelay() {
+        yield return new WaitForSeconds(teleportDelay);
+        playerTransform.position = playerTransform.position + (linkedTeleporter.position - this.transform.position);
+    }
+}
