3adf07c8f6d37dcb6363c272e431271cf43788bc
Olivier Filippelli <olivier.filippelli@polymtl.ca>
2017-03-12 04:33:20 -0400

    Ajout TileBegin + TileEnd (Besoin modif. Script TileEnd)

---
+++ McGameJam2017/Assets/Scripts/Game/Tiles/TileEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileEnd : MonoBehaviour {
+
+    private GameObject playerVR;
+
+	// Use this for initialization
+	void Start () {
+        playerVR = GameObject.FindGameObjectWithTag("PlayerVR");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
