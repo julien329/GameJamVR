f50daec62daa899a6a1189ed1315fd26fd1aaef4
Olivier Filippelli <olivier.filippelli@polymtl.ca>
2017-03-11 20:12:51 -0500

    Ajout Prefabs et  Animations

---
+++ McGameJam2017/Assets/RumblingTileScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumblingTileScript : MonoBehaviour {
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "PlayerVR")
+        {
+            anim.SetBool("isPlayerOnTile", true);
+            Invoke("TileFall", 2);
+        }
+    }
+
+    void TiteFall()
+    {
+        anim.SetTrigger("TileFall");
+    }
+}
